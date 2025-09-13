cc6b50126394c0c02211e66643f74bddbd6ed562
foferys <foferys@gmail.com>
2025-09-13 15:39:41 +0200

    first commit

---
+++ src/main/java/com/foferys_journal/fofejournal/FofejournalApplication.java
@@ -0,0 +1,13 @@
+package com.foferys_journal.fofejournal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FofejournalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FofejournalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/foferys_journal/fofejournal/FofejournalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foferys_journal.fofejournal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FofejournalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
