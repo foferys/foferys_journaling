671bd45392865e5ffb4b3b59720d90a405733147
foferys <foferys@gmail.com>
2026-04-03 11:34:10 +0200

    FusaApiRequest come record

--- src/main/java/com/foferys_journal/fofejournal/models/FusaApiRequest.java
+++ src/main/java/com/foferys_journal/fofejournal/models/FusaApiRequest.java
@@ -6,6 +6,7 @@ import jakarta.validation.constraints.Size;
 /**
  * DTO per richieste API REST (POST/PUT) – solo titolo e contenuto in JSON.
  */
+/*
 public class FusaApiRequest {
 
     @NotEmpty(message = "Il titolo è obbligatorio")
@@ -28,5 +29,17 @@ public class FusaApiRequest {
 
     public void setContenuto(String contenuto) {
         this.contenuto = contenuto;
+    }
+}
+*/
+public record FusaApiRequest( @NotEmpty(message = "Il titolo è obbligatorio") String titolo, @Size(min = 10, max = 2000, message = "Il contenuto deve essere tra 10 e 2000 caratteri") String contenuto) {
+
+    // Mantiene compatibilità con il codice esistente che usa i getter JavaBean. Altrimenti si possono chiamare con .titolo() e .contenuto()
+    public String getTitolo() {
+        return titolo;
+    }
+
+    public String getContenuto() {
+        return contenuto;
     }
 }
